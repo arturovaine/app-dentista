b48f8b0296ad1e79402026ad00e9da615952cdae
Arturo Vaine <arturo.vaine@gmail.com>
2022-08-29 11:45:41 -0300

    feat: simple main-body component

--- app-frontend/src/App.tsx
+++ app-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import MainBody from './components/MainBody';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MainBody />
     </div>
   );
 }
