8e7e866cc31a1d60fdd0f49df9f8f5dd43580629
Arturo Vaine <arturo.vaine@gmail.com>
2022-09-09 20:10:53 -0300

    chore: modal component (state, button..)

--- app-frontend/src/context/Consumer.tsx
+++ app-frontend/src/context/Consumer.tsx
@@ -1,39 +1,51 @@
+import { useState } from 'react';
 import Context from './Context';
 import { TableHeader } from '../helpers/helpers';
 import ButtonEdit from '../components/ButtonEdit';
+import Modal from '../components/Modal';
 
-const Consumer = () => (
-  <Context.Consumer>
-    {(context: any) => (
-      <span>
-        Renderizando o context:
-        <br /><br />
-        <table>
-          <thead>
-            { TableHeader() }
-          </thead>
-          <tbody>
-            {
-              context.map((item: any, index: number) => (
-                <tr key={ index }>
-                  <td>{item.appointmentDate} &nbsp; </td>
-                  <td>{item.patientLastName} &nbsp; </td>
-                  <td>{item.patientFirstName} &nbsp; </td>
-                  <td>{item.paymentValue.toFixed(2)} &nbsp; </td>
-                  <td>{item.paymentInstallment} &nbsp; </td>
-                  <td>{item.paymentFraction} &nbsp; </td>
-                  <td>{item.paymentTotalValue.toFixed(2)} &nbsp; </td>
-                  <td>
-                  <ButtonEdit />
-                  </td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </span>
-    )}
-  </Context.Consumer>
-);
+
+function Consumer() {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <Context.Consumer>
+      {(context: any) => (
+        <span>
+          Renderizando o context:
+          <br /><br />
+          <button
+            className="openModalBtn"
+            onClick={()=> {setOpenModal(true)}}
+          >Open Modal</button>
+          { openModal && <Modal /> }
+          <br /><br />
+          <table>
+            <thead>
+              { TableHeader() }
+            </thead>
+            <tbody>
+              {
+                context.map((item: any, index: number) => (
+                  <tr key={ index }>
+                    <td>{item.appointmentDate} &nbsp; </td>
+                    <td>{item.patientLastName} &nbsp; </td>
+                    <td>{item.patientFirstName} &nbsp; </td>
+                    <td>{item.paymentValue.toFixed(2)} &nbsp; </td>
+                    <td>{item.paymentInstallment} &nbsp; </td>
+                    <td>{item.paymentFraction} &nbsp; </td>
+                    <td>{item.paymentTotalValue.toFixed(2)} &nbsp; </td>
+                    <td>
+                    <ButtonEdit />
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </span>
+      )}
+    </Context.Consumer>)
+};
 
 export default Consumer;
